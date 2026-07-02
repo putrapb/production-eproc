463b4d7750ba786fa1a526b8a327b91deea47541
Putra Pertama <putrapb281@gmail.com>
2026-07-02 12:15:12 +0700

    perbaiki nama menu di sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -74,8 +74,8 @@
         <svg width="18" height="18" fill="none" stroke="#fff" stroke-width="2" viewBox="0 0 24 24"><path d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2"/><rect x="9" y="3" width="6" height="4" rx="1"/><path d="M9 12h6M9 16h4"/></svg>
       </div>
       <div class="sidebar-logo-text">
-        Helpdesk E-Procurement
-        <span>IT Infrastructure Projects Management</span>
+        Helpdesk
+        <span>E-Procurement</span>
       </div>
     </div>
 
